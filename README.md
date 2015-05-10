722de53f32de1565c68c641553d3432878c7ec51
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-05-09 22:53:44 -0700

    stuck on move method

--- README.md
+++ README.md
@@ -11,11 +11,25 @@ May 8 --- I think I finally figured out how to run the game.  I need to have the
 
 6:10pm --- I don't know how to do anything
 
+7:30pm --- Daniel was able to walk me through the game and give me an idea on how to use OOP with this project.  I'm gaining a pretty good grasp of the concepts, but I'm still fuzzy on how to connect all of the dots.
+
+May 9 --- Beginning in the morning, I'm drawing out a diagram of the game and seeing if this helps me.
+
+11:00pm --- Daniel was helping me through chat, but I didn't seem to fully grasp how to change the scope between Board/Player.  I'm close - it's just one line that's messing me up.  I'm going in to GA tomorrow to get help from the TA
+
+2:30pm --- I'm styling my page to knock it out of the way so that all my time tomorrow will be spent dealing with js and the logic behind the game.
+
+3:00pm --- I figured out how to change the background image when each square is clicked
+
 
 
 
 ### Technologies Used
 *What technologies did you use while developing this project?*
+1) I used CSS Font Stack website to help with font for my Tic-Tac-Toe label
+2) HTML, CSS, JavaScript, and jQuery were all used for this project
+3) I used w3 schools font color list to help with coloring my words
+4) I used picture resize website for my background images
 
 ###Existing features
 *What features does your new tic-tac-toe app have?*
--- app.js
+++ app.js
@@ -1,94 +1,58 @@
-//set the turn as a global variable
-var turn;
 $(document).ready(function() {
 
 //create a Game which has 2 players
   function Game() {
-    var playerX = new Player("X");
-    var playerO = new Player("O");
+    this.playerX = new Player("X");
+    this.playerO = new Player("O");
+    this.board = new Board();
   }
 
 //
   Game.prototype.nextPlayer = function(turn) {
-    if (turn === "X") {
-      return turn = "O";
+    this.turn = turn;
+    if (this.turn === "O") {
+      $(".cell").on("click", function(event) {
+        this.turn = "X";
+      })
     }
-    else if (turn === "O") {
-      return turn === "X";
+    else if (this.turn === "X") {
+      // this.board.move("X", $("#sq1"));
+      $(".cell").on("click", function(event) {
+        $(this).addClass("X");
+        this.turn = "O";
+      })
     }
     else {
-      return "I messed up the nextPlayer method";
+      console.log("isn't working");
     }
+
   };
 
-  /* `Game.prototype.init` kicks off a new game with a board and two players
-  confirm that the user would like to play tic-tac-toe
-  if user rejects playing tic tac toe, then I prompt them to make sure that they don't want to play
-  */
+  
   Game.prototype.init = function() {
-    var question = confirm("Would you like to play tic-tac-toe?");
-    if (question === true) {
-      turn = "X";
-    }
-    else {
-      var question2 = prompt("Are you sure?");
-        if (question2 === "yes") {
-          alert("Enjoy your day then!");
-        }
-    }
-    
-/*when I run a click function in the global scope, I have it run the checkWinner method to see if a winner has been
-found.  the checkWinner needs to check for the winner and will alert if a winner is found
-*/
+    var turn = "X";
+    game.nextPlayer(turn);
+  }
+
   Game.prototype.checkWinner = function() {
     
     }
 
-}
   
   function Player(team) {
-    this.team = team;  // individual memory
+    this.team = team;
   };
-
-  Player.prototype.nextTurn = function(team) { //shared memory
-    $cells = $(".cell");
-    if (team === "X") {
-      $cells.on("click", function() {
-        $cells.addClass("X");
-        return turn = "O";
-      })
-    }
-    else if (team === "O") {
-      $cells.on("click", function() {
-        $cells.addClass("O");
-      })
-      return turn = "X";
-    }
-  }
 
   // A starter Board constructor.
   function Board() {
-    $(".cell").addClass("cell");
+    this.$cells = $(".cell")
   };
 
+  Board.prototype.move = function(player, $cell) {
+    $cell.text(player);
+  }
 
-/* have each cell listen for a click which will then run a function that switches player
-*/
-  $(".cell").on("click", function() {
-    $(this).text("X");
-    player.nextTurn(turn);
-  })
-  
-  $("#Button").on("click", clearBoard);
-    function clearBoard() {
-      var board = new Board();
-    /*set the html in board.  here i will create a new game by saying var board = new Board() which will then
-      look into the Board constructor for the layout of the board.  Board constructor should have the innerHTML
-    */
-    } 
   // Start the game!
   var game = new Game();
-  var player = new Player();
   game.init();
-  game.checkWinner();
 });
